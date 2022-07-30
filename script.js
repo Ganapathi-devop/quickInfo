d4242df3f35fcdb88c7d7f999366bf2dd3fc8805
ganapathi-devop <ganapathy1red1@gmail.com>
2022-07-30 15:32:59 +0530

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    setInterval(preloaderTimeout, 5000)
+    function preloaderTimeout(){
+        $('.preloader-wrapper').fadeOut(100);
+        $('.preloader-wrapper').addClass('invisible')
+        $('.login-section').removeClass('d-none')
+    }
+});
